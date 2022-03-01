ecdd4f69a7a81012483e1b1805f0b6bb78acd2d5
Kauã Chaves Nielson <kauaprofessionalcontact@gmail.com>
2022-03-01 16:12:02 -0300

    Added an audio when the timer ends

--- src/timer_actions/endTimer.js
+++ src/timer_actions/endTimer.js
@@ -1,10 +1,14 @@
 import { id_interval, timer_started, updateTimerForBrowser, Button } from "./startTimer.js"
 import { ColorBorder } from "../colorCircleBorder.js"
 
+const Audio = document.querySelector("#audio")
+
 function endTimer() {
     updateTimerForBrowser()
     ColorBorder("#d85d5d", "#d85d5d", "Terminou o tempo")
     clearInterval(id_interval)
+    Audio.play()
+    Audio.volume -= 0.6
     Button.style.cursor = "default"
 }
 
