3efd7d9021c62432780fc96c6fabf1f53827fdef
Kauacnok <kauaprofessionalcontact@gmail.com>
2022-04-10 14:52:07 -0300

    Modifications to create the theme switcher and the theme switcher script

---
+++ src/toggleButton.js
@@ -0,0 +1,30 @@
+const ToggleButton = document.getElementsByClassName("toggleButton")
+const theme = window.localStorage.getItem("theme")
+
+ToggleButton[2].addEventListener("click", themeSwitcher)
+
+let isLightTheme = ""
+function configureTheToggleButton() {
+	if (isLightTheme) {
+		ToggleButton[0].style.justifyContent = "right"
+		ToggleButton[2].src = "../assets/moon.svg"
+	} else {
+		ToggleButton[0].style.justifyContent = "left"
+		ToggleButton[2].src = "../assets/sun.svg"
+	}
+}
+
+if (theme == "light") document.body.classList.add("light")
+
+configureTheToggleButton()
+
+function themeSwitcher() {
+	isLightTheme = document.body.classList.toggle("light")
+   	if (theme == "light") {
+  		window.localStorage.setItem("theme", "dark")
+  		configureTheToggleButton()
+   	} else {
+   		window.localStorage.setItem("theme", "light")
+   		configureTheToggleButton()
+   	}
+}
